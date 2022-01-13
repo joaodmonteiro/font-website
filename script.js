4d7dab1a04a56d3929610267f859843657f2c829
joaodmonteiro <joaodmonteiro10@gmail.com>
2022-01-13 12:34:39 +0000

    variable font change weights when hovered

---
+++ script.js
@@ -0,0 +1,12 @@
+let word = document.querySelector('#word');
+let inputText = document.querySelector('.inputText');
+let submitButton = document.querySelector('.submitButton');
+
+submitButton.addEventListener("click", function() {
+
+    // for(let i=0; i<inputText.value.length; i++)
+    // {
+    //     let char = document.createElement('li');
+    //     char.textContent = inputText.value
+    // }
+});
