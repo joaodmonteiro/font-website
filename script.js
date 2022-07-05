15037c48a38ab2ce5ac2ffc9658cf0dbdebe8337
joaodmonteiro <joaodmonteiro10@gmail.com>
2022-07-05 11:55:52 +0100

    animation at the start

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-let word = document.querySelector('#word');
-let inputText = document.querySelector('.inputText');
+let word = document.querySelector("#word");
+let inputText = document.querySelector(".inputText");
 
-document.addEventListener("keyup", function() {
-    word.innerHTML = "";
+console.log(word.children[0]);
 
-    for(let i=0; i<inputText.value.length; i++)
-    {
-        let char = document.createElement('li');
-        char.textContent = inputText.value[i];
-        word.appendChild(char);
-    }
+setTimeout(() => {
+  animateChar(0);
+}, 200);
+
+function animateChar(index) {
+  word.children[index].classList.add("animated");
+
+  setTimeout(() => {
+    word.children[index].classList.remove("animated");
+  }, 300);
+
+  if (index < word.children.length - 1) {
+    setTimeout(() => {
+      animateChar(index + 1);
+    }, 100);
+  }
+}
+
+document.addEventListener("keyup", function () {
+  word.innerHTML = "";
+
+  for (let i = 0; i < inputText.value.length; i++) {
+    let char = document.createElement("li");
+    char.textContent = inputText.value[i];
+    word.appendChild(char);
+  }
 });
